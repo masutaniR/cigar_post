d4e9d8abec914da8e7411b3fb37d15456276bfc1
masutaniR <popohu87@gmail.com>
2021-03-06 07:12:09 +0000

    [Add] post showページ、destroy

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,11 +1,11 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:destroy]
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -15,22 +15,26 @@ class Public::PostsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def index
+    @posts = Post.page(params[:page]).reverse_order
   end
-  
+
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to user_path(current_user)
   end
-  
+
   private
     def post_params
       params.require(:post).permit(:body, :post_image, :category)
     end
-    
+
     def ensure_correct_user
       @post = Post.find(params[:id])
       unless @post.user == current_user
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  validates :body, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 100 }
+  validates :category, presence: true
   attachment :post_image
   enum category: { senryu: 0, tanka: 1, free_haiku: 2 }
 
