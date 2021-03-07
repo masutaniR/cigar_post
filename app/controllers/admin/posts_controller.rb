e874c16a45d94753368029429b8769d292257267
masutaniR <popohu87@gmail.com>
2021-03-07 09:01:19 +0000

    [Add] 管理側投稿一覧

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,18 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    if params[:user_id].present?
+      @user = User.find(params[:user_id])
+      @posts = Post.where(user_id: @user.id).reverse_order.page(params[:page])
+    else
+      @posts = Post.all.reverse_order.page(params[:page])
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+  end
 end
