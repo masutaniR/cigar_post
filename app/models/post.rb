fcc30994a6f0c809e40e8f6cbb4f0ea14db3d2c0
masutaniR <popohu87@gmail.com>
2021-03-08 03:54:51 +0000

    [Add] 通知

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   # ユーザが投稿にコメント済みか確認する
   def commented_by?(user)
@@ -16,5 +17,43 @@ class Post < ApplicationRecord
   # ユーザが投稿にいいね済みか確認する
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
+  end
+
+  # いいね通知
+  def create_notification_like(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ?", current_user.id, user_id, id, 'like'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
+
+  # コメント通知
+  def create_notification_comment(current_user, comment_id)
+    temp_ids = PostComment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment(current_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      post_id: id,
+      post_comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
   end
 end
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -5,4 +5,5 @@ class PostComment < ApplicationRecord
 
   belongs_to :user
   belongs_to :post
+  has_many :notifications, dependent: :destroy
 end
