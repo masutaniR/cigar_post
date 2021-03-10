cbab17e6fd7c6ae05ae5d08d79c73d04a50012f6
masutaniR <popohu87@gmail.com>
2021-03-10 08:21:43 +0000

    [Add] 管理側コメント検索

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -6,4 +6,21 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   has_many :notifications, dependent: :destroy
+
+  # 管理側コメント検索
+  def self.comment_search_for(content)
+    PostComment.where("comment LIKE?", "%#{content}%")
+  end
+
+  # 管理側コメントカテゴリ検索
+  def self.category_search_for(content)
+    case content
+    when '川柳'
+      PostComment.where("category LIKE?", "0")
+    when '短歌'
+      PostComment.where("category LIKE?", "1")
+    when '自由律俳句'
+      PostComment.where("category LIKE?", "2")
+    end
+  end
 end
