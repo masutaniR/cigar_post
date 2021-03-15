db1fc03e6cecc85476757bab3cacf2bcc9c6628c
masutaniR <popohu87@gmail.com>
2021-03-15 10:13:45 +0000

    [Fix] 記入漏れ

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,8 +46,7 @@ class User < ApplicationRecord
   # フォロー通知
   def create_notification_follow(current_user)
     temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ?", current_user.id, id, 'follow'])
-    user = User.find(id)
-    if temp.blank? && user.follow_notice == true
+    if temp.blank?
       notification = current_user.active_notifications.new(
         visited_id: id,
         action: 'follow'
